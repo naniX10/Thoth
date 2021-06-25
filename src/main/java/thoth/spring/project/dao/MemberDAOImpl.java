39efaf9cfc9624292ef2b929b6520d0671d7572e
이용현 <apdlvmf556@naver.com>
2021-06-25 13:49:48 +0900

    회원가입및 로그인 로그아웃

---
+++ src/main/java/thoth/spring/project/dao/MemberDAOImpl.java
@@ -0,0 +1,37 @@
+package thoth.spring.project.dao;
+
+
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import thoth.spring.project.vo.Member;
+import thoth.spring.project.vo.Zipcode;
+
+import java.util.List;
+
+@Repository("mdao")
+public class MemberDAOImpl implements MemberDAO {
+
+    @Autowired private SqlSession sqlSession;
+
+    @Override
+    public int insertMember(Member m) {
+        return sqlSession.insert("member.insertMember", m);
+    }
+
+    @Override
+    public List<Zipcode> selectZipcode(String dong) {
+        return sqlSession.selectList("member.selectZipcode", dong);
+    }
+
+    @Override
+    public int selectOneUserid(String uid) {
+        return sqlSession.selectOne("member.checkUserid", uid);
+    }
+
+    @Override
+    public int selectLogin(Member m) {
+        return sqlSession.selectOne("member.checkLogin", m);
+    }
+}
